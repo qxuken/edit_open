1e2bacdbb277bfe819edf407ce604b52204dd1ef
Nasonov Viktor <qxuken@gmail.com>
2025-11-10 19:53:15 +0300

    WIP

--- lua/nvim.lua
+++ lua/nvim.lua
@@ -1,24 +1,138 @@
-local function create_server(host, port, on_connect)
-	local server = uv.new_tcp()
-	server:bind(host, port)
-	server:listen(128, function(err)
-		assert(not err, err) -- Check for errors.
-		local sock = uv.new_tcp()
-		server:accept(sock) -- Accept client connection.
-		on_connect(sock) -- Start reading messages.
-	end)
-	return server
-end
-local server = create_server("0.0.0.0", 0, function(sock)
-	sock:read_start(function(err, chunk)
-		assert(not err, err) -- Check for errors.
-		if chunk then
-			sock:write(chunk) -- Echo received messages to the channel.
-		else -- EOF (stream closed).
-			sock:close() -- Always close handles to avoid leaks.
-		end
-	end)
-end)
-print("TCP echo-server listening on port: " .. server:getsockname().port)
+local uv = require("luv")
+local utils = {}
 
-server:stop()
+local HOST = "127.0.0.1"
+local PORT = 47912
+
+local commands = {
+	-- { "open", leader_handle, follower_handle },
+}
+
+local G = {
+	peers = {},
+	role = {
+		type = "candidate",
+	},
+}
+
+local function recv_msg(err, data, addr, flags)
+	assert(not err, err)
+	print("[data]")
+	if data ~= nil then
+		print("len = " .. string.len(data))
+		print(data)
+	else
+		print("nil")
+	end
+	print("[addr]")
+	if addr ~= nil then
+		utils.dump(addr)
+	else
+		print("nil")
+	end
+	print("[flags]")
+	if flags ~= nil then
+		utils.dump(flags)
+	else
+		print("nil")
+	end
+	if G.role.type == "leader" and data ~= nil and addr ~= nil then
+		G.role.socket:send(data, addr.ip, addr.port, function(err1)
+			print(err1)
+		end)
+	end
+end
+
+local function try_leader()
+	print("try_leader")
+	local role = {
+		type = "leader",
+		socket = uv.new_udp(),
+	}
+	local ret, err, code
+	ret, err, code = role.socket:bind(HOST, PORT)
+	if ret ~= 0 then
+		return err, code
+	end
+	ret, err, code = role.socket:recv_start(recv_msg)
+	if ret ~= 0 then
+		return err, code
+	end
+	print("udp server listening on port: " .. PORT)
+	G.role = role
+	return nil, nil
+end
+
+local function try_follower()
+	print("try_follower")
+	local socket = uv.new_udp()
+	local ret, err, code
+	ret, err, code = socket:bind(HOST, 0)
+	assert(ret == 0 and not code, err)
+	ret, err, code = socket:connect(HOST, PORT)
+	assert(ret == 0 and not code, err)
+	local timer = uv.new_timer()
+	timer:start(0, 1000, function()
+		socket:send("ping" .. uv.now(), nil, nil)
+	end)
+	ret, err, code = socket:recv_start(recv_msg)
+	assert(ret == 0 and not code, err)
+	print("udp client open on port: " .. socket:getsockname().port)
+	G.role = {
+		type = "follower",
+		socket = socket,
+		timer = timer,
+	}
+end
+
+local function run_comms(retries)
+	local retries_left = retries and retries - 1 or 3
+	print("run_comms: " .. retries_left)
+	if retries_left == 0 then
+		return false, "No more retries, quiting"
+	end
+	local err
+	err = try_leader()
+	if err ~= nil then
+		print(err)
+		err = try_follower()
+		if err ~= nil then
+			print(err)
+			return run_comms(retries_left)
+		end
+	end
+	return true, nil
+end
+
+-- Utils
+
+function utils.dump(t, indent, seen)
+	indent, seen = indent or "", seen or {}
+	if seen[t] then
+		print(indent .. "*RECURSION*")
+		return
+	end
+	seen[t] = true
+	for k, v in pairs(t) do
+		if type(v) == "table" then
+			print(("%s[%s] = {"):format(indent, tostring(k)))
+			utils.dump(v, indent .. "  ", seen)
+			print(indent .. "}")
+		else
+			print(("%s[%s] = %s"):format(indent, tostring(k), tostring(v)))
+		end
+	end
+end
+
+-- Hot Loop
+
+print(run_comms())
+-- if not ok then
+-- 	print("Comms failed: " .. err and err or "unknown")
+-- end
+
+-- uv.signal_start(uv.constants.SIGTERM, function(signame)
+-- 	print(signame) -- string output: "sigterm"
+-- end)
+
+uv.run()
